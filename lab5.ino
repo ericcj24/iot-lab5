df7a432f76bbf4933eb71c3846f4144db4aeebb5
MaduraSkye <kevin.e.zhang@gmail.com>
2024-12-01 17:07:36 -0800

    Added code for temperature sensor and air quality sensor

--- lab5.ino
+++ lab5.ino
@@ -1,44 +1,69 @@
 #include "Adafruit_GPS.h"
 #include "PulseOximeter.h"
+#include "MQ135.h"
 
+//Initialize Pulse Oxi Variables
 #define PULSE_OXI_PIN 2
+
+//Initialize Temperature Sensor Variables
+const int lm35_pin = A1;
+int temp_adc_val;
+float temp_val;
+
+//Initialize Air Quality Sensor Variables
+const int air_quality_pin = A0;
+float ppm;
+
+//Initialize GPS Variables
+string gpsResult;
+
+
+
 PulseOximeter pox;
-
-
 Adafruit_GPS GPS;
+MQ135 gasSensor = MQ135(air_quality_pin);
 
 
 void setup() {
   Serial.begin(9600);
-
   pinMode(PULSE_OXI_PIN, INPUT);
-
 }
+
+
 void loop() {
+  temp_adc_val = analogRead(lm35_pin);	/* Read Temperature */
+  temp_val = (temp_adc_val * 4.88);	/* Convert adc value to equivalent voltage */
+  temp_val = (temp_val/10);	/* LM35 gives output of 10mv/°C */
+  Serial.print("Temperature = ");
+  Serial.print(temp_val);
+  Serial.println(" Degree Celsius");
+
+  ppm = gasSensor.getPPM();
+  Serial.print("C02 in air: ");
+  Serial.print(ppm);
+  Serial.println("ppm");
+
   if (Serial.available() > 0) {	
-    string gpsResult = GPS.read();
-    Serial.println("gps result");
-    Serial.print(gpsResult);
-
-    
-
+    gpsResult = GPS.read();
+    Serial.print("GPS Result: ");
+    Serial.println(gpsResult);
   } else {
-      Serial.print("not available");
+      Serial.println("GPS Not Available");
   }
 
   if (pox.begin()) {
-        // get the readings and print them out
       string oximeterResult = pox.getHeartRate();
-      Serial.println("poximeter heartrate");
-      Serial.print(oximeterResult);
+      Serial.print("Poximeter Heartrate: ");
+      Serial.println(oximeterResult);
 
-      Serial.println("poximeter spO2");
-      Serial.print(pox.getSpO2());
+      Serial.print("Poximeter SpO2: ");
+      Serial.println(pox.getSpO2());
     } else { 
-        // do something. Hint: you can print some messages
+      Serial.println("Poximeter Reading Not Available");
     }
-
-
+  
+  
   delay(1000);
+
 }
 
